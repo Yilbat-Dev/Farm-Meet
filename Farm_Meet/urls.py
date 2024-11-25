bd4aac5b2747353b5fc028ceeba7697ff83cb634
Yilbat <mudnoethankgod.com>
2024-11-25 07:44:44 +0100

    email

--- Farm_Meet/urls.py
+++ Farm_Meet/urls.py
@@ -30,6 +30,7 @@ schema_view = get_schema_view(
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
+        developer_email =  'mudnoethankgod@gmail.com'
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
